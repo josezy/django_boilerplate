160b535781fb236e75ef0eb732e332cf5cc74ac7
Jose B <jose.zdy@gmail.com>
2020-08-15 17:34:49 -0500

    basic object template

--- news_django/news/views.py
+++ news_django/news/views.py
@@ -1,8 +1,10 @@
 from core.view_utils import BaseView
+from news.models import NewsItem
 
 
 class Home(BaseView):
     template = "news/home.html"
 
     def get(self, request, *args, **kwargs):
-        return self.render_template(request)
+        context = {"news_items": NewsItem.objects.all()}
+        return self.render_template(request, context)
